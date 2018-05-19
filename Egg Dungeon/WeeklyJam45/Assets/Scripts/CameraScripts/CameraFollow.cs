3fd70e37744fa3a4c12e6364bfdf61cd9ebe61a6
dtunnel <damontunnell@gmail.com>
2018-05-19 19:52:27 -0400

    TileMapped Rooms
    Created test room using the tile pallet for all room objects. Added custom colliders and added temporary collision system.

---
+++ Egg Dungeon/WeeklyJam45/Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset; 
+
+	// Use this for initialization
+	void Start () {
+
+        // Initializes target as player
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        /*
+        Vector3 desiredPosition = target.transform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+	    */
+        transform.position = target.transform.position + offset;
+    }
+}
